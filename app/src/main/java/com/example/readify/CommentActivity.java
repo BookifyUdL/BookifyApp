7ab58fc90d865573b7d04f2043c662faa13f6dd6
rspaimoc2 <raduspaimoc@gmail.com>
2019-11-05 20:02:20 +0100

    Gif added

--- app/src/main/java/com/example/readify/CommentActivity.java
+++ app/src/main/java/com/example/readify/CommentActivity.java
@@ -1,15 +1,30 @@
 package com.example.readify;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.os.BuildCompat;
+import androidx.core.view.inputmethod.EditorInfoCompat;
+import androidx.core.view.inputmethod.InputConnectionCompat;
+import androidx.core.view.inputmethod.InputContentInfoCompat;
 
 import android.content.ClipDescription;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
 import android.webkit.MimeTypeMap;
 import android.webkit.WebView;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.gsconrad.richcontentedittext.RichContentEditText;
 
 import java.io.File;
@@ -18,6 +33,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URL;
 
 public class CommentActivity extends AppCompatActivity {
 
@@ -29,9 +45,77 @@ public class CommentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comment);
         setupRichContentEditText();
+
     }
 
-    private void setupRichContentEditText() {
+    public void setWebView(Uri uri){
+        //WebView webView = findViewById(R.id.webView);
+        //webView.loadUrl(uri.toString());
+        ImageView img = findViewById(R.id.imageView);
+        Glide.with(getApplicationContext()) // replace with 'this' if it's in activity
+        .load(uri.toString())
+        .asGif()
+        .error(R.drawable.angry) // show error drawable if the image is not a gif
+        .into(img);
+    }
+
+    private void setupRichContentEditText(){
+        TextView textView = findViewById(R.id.textamen);
+        RichEditText editText = new RichEditText(getApplicationContext(), this);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        //editText.setBackground(R.color.colorBlank);
+        editText.setLayoutParams(params);
+        RelativeLayout relativeLayout = findViewById(R.id.edit_text_lay);
+        relativeLayout.addView(editText);
+
+        /*EditText editText = findViewById(R.id.edit_text);
+        editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    InputConnection ic = editText.onCreateInputConnection(new EditorInfo());
+                    keyboard.setInputConnection(ic); // custom keyboard method
+                }
+            }
+        });
+        //editText.onCreateInputConnection()
+        /*editText = new EditText(this) {
+            @Override
+            public InputConnection onCreateInputConnection(EditorInfo editorInfo) {
+                final InputConnection ic = super.onCreateInputConnection(editorInfo);
+                EditorInfoCompat.setContentMimeTypes(editorInfo,
+                        new String [] {"image/png"});
+
+                final InputConnectionCompat.OnCommitContentListener callback =
+                        new InputConnectionCompat.OnCommitContentListener() {
+                            @Override
+                            public boolean onCommitContent(InputContentInfoCompat inputContentInfo,
+                                                           int flags, Bundle opts) {
+                                // read and display inputContentInfo asynchronously
+                                if (BuildCompat.isAtLeastNMR1() && (flags &
+                                        InputConnectionCompat.INPUT_CONTENT_GRANT_READ_URI_PERMISSION) != 0) {
+                                    try {
+                                        inputContentInfo.requestPermission();
+                                    }
+                                    catch (Exception e) {
+                                        return false; // return false if failed
+                                    }
+                                }
+
+                                // read and display inputContentInfo asynchronously.
+                                // call inputContentInfo.releasePermission() as needed.
+
+                                return true;  // return true if succeeded
+                            }
+                        };
+                return InputConnectionCompat.createWrapper(ic, editorInfo, callback);
+            }
+        };*/
+
+    }
+
+    /*private void setupRichContentEditText() {
         RichContentEditText editText = findViewById(R.id.edit_text);
         // The following line sets the listener that is called when rich content is received
         editText.setOnRichContentListener(new RichContentEditText.OnRichContentListener() {
@@ -62,7 +146,7 @@ public class CommentActivity extends AppCompatActivity {
                 }
             }
         });
-    }
+    }*/
 
     public boolean writeToFileFromContentUri(File file, Uri uri) {
         if (file == null || uri == null) return false;
---
+++ app/src/main/java/com/example/readify/RichEditText.java
@@ -0,0 +1,120 @@
+package com.example.readify;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+import android.widget.TextView;
+
+import androidx.appcompat.widget.AppCompatEditText;
+import androidx.core.os.BuildCompat;
+import androidx.core.view.inputmethod.EditorInfoCompat;
+import androidx.core.view.inputmethod.InputConnectionCompat;
+import androidx.core.view.inputmethod.InputContentInfoCompat;
+
+public class RichEditText extends AppCompatEditText {
+    private String[] imgTypeString;
+    private KeyBoardInputCallbackListener keyBoardInputCallbackListener;
+    private CommentActivity activity;
+
+    public RichEditText(Context context) {
+        super(context);
+        initView();
+    }
+
+    public RichEditText(Context context, CommentActivity activity) {
+        super(context);
+        this.activity = activity;
+        initView();
+
+
+
+    }
+
+    public RichEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView();
+    }
+
+    private void initView() {
+        imgTypeString = new String[]{"image/png",
+                "image/gif",
+                "image/jpeg",
+                "image/webp"};
+    }
+
+    /*public Uri getUri(){
+        /
+    }*/
+
+    @Override
+    public InputConnection onCreateInputConnection(EditorInfo outAttrs) {
+        final InputConnection ic = super.onCreateInputConnection(outAttrs);
+        EditorInfoCompat.setContentMimeTypes(outAttrs,
+                imgTypeString);
+        return InputConnectionCompat.createWrapper(ic, outAttrs, callback);
+    }
+
+
+
+    final InputConnectionCompat.OnCommitContentListener callback =
+            new InputConnectionCompat.OnCommitContentListener() {
+                private Uri uri;
+
+                public Uri getUri(){
+                    return uri;
+                }
+
+                @Override
+                public boolean onCommitContent(InputContentInfoCompat inputContentInfo,
+                                               int flags, Bundle opts) {
+
+                    // read and display inputContentInfo asynchronously
+                    this.uri = inputContentInfo.getContentUri();
+                    activity.setWebView(this.uri);
+                    //textView.setText(this.uri.toString());
+                    if (BuildCompat.isAtLeastNMR1() && (flags &
+                            InputConnectionCompat.INPUT_CONTENT_GRANT_READ_URI_PERMISSION) != 0) {
+                        try {
+                            inputContentInfo.requestPermission();
+                        } catch (Exception e) {
+                            return false; // return false if failed
+                        }
+                    }
+                    boolean supported = false;
+                    for (final String mimeType : imgTypeString) {
+                        if (inputContentInfo.getDescription().hasMimeType(mimeType)) {
+                            supported = true;
+                            break;
+                        }
+                    }
+                    if (!supported) {
+                        return false;
+                    }
+
+                    if (keyBoardInputCallbackListener != null) {
+                        keyBoardInputCallbackListener.onCommitContent(inputContentInfo, flags, opts);
+                    }
+                    return true;  // return true if succeeded
+                }
+            };
+
+    public interface KeyBoardInputCallbackListener {
+        void onCommitContent(InputContentInfoCompat inputContentInfo,
+                             int flags, Bundle opts);
+    }
+
+    public void setKeyBoardInputCallbackListener(KeyBoardInputCallbackListener keyBoardInputCallbackListener) {
+        this.keyBoardInputCallbackListener = keyBoardInputCallbackListener;
+    }
+
+    public String[] getImgTypeString() {
+        return imgTypeString;
+    }
+
+    public void setImgTypeString(String[] imgTypeString) {
+        this.imgTypeString = imgTypeString;
+    }
+}
