1ee5587ea77523eb0c972a493ff5cc5607c7ad4a
Marc Viladegut <viladegut_81@hotmail.com>
2019-11-07 11:56:17 +0100

    Create and implements a login and logout with Facebook and Google accounts

--- app/src/main/java/com/example/readify/Login/LoginActivity.java
+++ app/src/main/java/com/example/readify/Login/LoginActivity.java
@@ -1,11 +1,24 @@
 package com.example.readify.Login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.animation.Animator;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
+
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewPropertyAnimator;
 import android.view.Window;
@@ -14,25 +27,63 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import static android.view.View.GONE;
 import static android.view.View.VISIBLE;
 
+import com.example.readify.FirstTimeForm.FirstTimeFormActivity;
+import com.example.readify.MainActivity;
+import com.example.readify.MockupsValues;
 import com.example.readify.R;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.felipecsl.gifimageview.library.GifImageView;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import org.apache.commons.io.IOUtils;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "FacebookLogin";
+    private static final int RC_SIGN_IN = 9001;
+    private static final int GOOGLE_SIGN_OUT = 4001;
+    private static final int FB_SIGN_OUT = 4002;
 
     private ImageView bookIconImageView;
     private TextView bookITextView;
     private ProgressBar loadingProgressBar;
     private RelativeLayout rootView, afterAnimationView;
     private GifImageView gifImageView;
+    private CallbackManager callbackManager;
+    private LoginButton loginFacebook;
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+    private SharedPreferences pref;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -43,20 +94,10 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         initViews();
 
-        try{
-            InputStream inputStream = getAssets().open("book_background.gif");
-            byte[] bytes = IOUtils.toByteArray(inputStream);
-            gifImageView.setBytes(bytes);
-            gifImageView.startAnimation();
-        } catch (IOException e) {
-
-        }
-
+        // Create an initial animation
         new CountDownTimer(2000, 1000) {
-
             @Override
             public void onTick(long millisUntilFinished) {
-
             }
 
             @Override
@@ -68,6 +109,43 @@ public class LoginActivity extends AppCompatActivity {
                 startAnimation();
             }
         }.start();
+
+        // Create an animated background with gif
+        try {
+            InputStream inputStream = getAssets().open("book_background.gif");
+            byte[] bytes = IOUtils.toByteArray(inputStream);
+            gifImageView.setBytes(bytes);
+            gifImageView.startAnimation();
+        } catch (IOException ignored) {}
+
+        // Initialize Google Login button
+        findViewById(R.id.logingoogle).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signInGoogle();
+                loadingProgressBar.setVisibility(VISIBLE);
+            }
+        });
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        // Initialize Facebook Login button
+        loginFacebook.setReadPermissions("email", "public_profile");
+        loginFacebook.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                signInFacebook(loginResult);
+            }
+
+            @Override
+            public void onCancel() {}
+
+            @Override
+            public void onError(FacebookException error) {}
+        });
     }
 
     private void initViews() {
@@ -77,17 +155,147 @@ public class LoginActivity extends AppCompatActivity {
         rootView = findViewById(R.id.rootView);
         afterAnimationView = findViewById(R.id.afterAnimationView);
         gifImageView = findViewById(R.id.gifImageView);
+        mAuth = FirebaseAuth.getInstance();
+        callbackManager = CallbackManager.Factory.create();
+        loginFacebook = findViewById(R.id.login_button);
+        pref = getSharedPreferences("com.example.readify", Context.MODE_PRIVATE);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Show the login components when the user returns to start
+        afterAnimationView.setVisibility(VISIBLE);
+
+        // Pass the activity result back to the Facebook SDK
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+
+        // Google Sign In was successful, authenticate with Firebase
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException ignored) {}
+
+        } else if (requestCode == GOOGLE_SIGN_OUT) {
+            logOutGoogle();
+        } else if (requestCode == FB_SIGN_OUT) {
+            logOutFacebook();
+        }
+    }
+
+    // Google SignIn
+    private void signInGoogle() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    // Function to login with Google
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            //Put the information about google to sharedpreferences
+                            pref.edit().putString("com.example.readify.uid", user.getUid()).apply();
+                            pref.edit().putString("com.example.readify.name", user.getDisplayName()).apply();
+                            pref.edit().putString("com.example.readify.email", user.getEmail()).apply();
+                            new DownloadImagesTask().execute(user.getPhotoUrl().toString());
+
+                            MockupsValues.setContext(LoginActivity.this);
+
+                            //Start activity with a Google logOut
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivityForResult(intent, GOOGLE_SIGN_OUT);
+                            loadingProgressBar.setVisibility(GONE);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication Failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    // Google SignOut
+    private void logOutGoogle() {
+        // Firebase sign out
+        mAuth.signOut();
+
+        // Google LogOut
+        mGoogleSignInClient.signOut();
+        mGoogleSignInClient.revokeAccess();
+    }
+
+    // Facebook SignIn
+    private void signInFacebook(LoginResult loginResult){
+        Log.d(TAG, "facebook:onSuccess:" + loginResult);
+        handleFacebookAccessToken(loginResult.getAccessToken());
+    }
+
+    // Function to login with Facebook
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+        afterAnimationView.setVisibility(GONE);
+        loadingProgressBar.setVisibility(VISIBLE);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            pref.edit().putString("com.example.readify.name", user.getDisplayName()).apply();
+                            pref.edit().putString("com.example.readify.uid", user.getUid()).apply();
+                            pref.edit().putString("com.example.readify.email", user.getEmail()).apply();
+                            new DownloadImagesTask().execute((user.getPhotoUrl().toString() + "?type=large"));
+
+                            Intent intent = new Intent(LoginActivity.this, FirstTimeFormActivity.class);
+                            startActivityForResult(intent, FB_SIGN_OUT);
+                            loadingProgressBar.setVisibility(GONE);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            afterAnimationView.setVisibility(VISIBLE);
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    // Facebook LogOut
+    private void logOutFacebook(){
+        // Firebase LogOut
+        mAuth.signOut();
+
+        // Facebook LogOut
+        LoginManager.getInstance().logOut();
+    }
+
+    // Animation when the app is open
     private void startAnimation() {
         ViewPropertyAnimator viewPropertyAnimator = bookIconImageView.animate();
-        //viewPropertyAnimator.x(50f);
         viewPropertyAnimator.y(100f);
         viewPropertyAnimator.setDuration(1000);
         viewPropertyAnimator.setListener(new Animator.AnimatorListener() {
+
             @Override
             public void onAnimationStart(Animator animation) {
-
             }
 
             @Override
@@ -98,13 +306,45 @@ public class LoginActivity extends AppCompatActivity {
 
             @Override
             public void onAnimationCancel(Animator animation) {
-
             }
 
             @Override
             public void onAnimationRepeat(Animator animation) {
-
             }
         });
+    }
+
+    // Class to provide a Bitmap from URL (Public access)
+    public class DownloadImagesTask extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            return getBitmapFromURL(strings[0]);
+        }
+
+        @SuppressLint("WrongThread")
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            result.compress(Bitmap.CompressFormat.PNG, 100, baos); //bm is the bitmap object
+            byte[] b = baos.toByteArray();
+            pref.edit().putString("com.example.readify.photo", Base64.encodeToString(b, Base64.DEFAULT)).apply();
+        }
+
+
+        private Bitmap getBitmapFromURL(String src) {
+            try {
+                URL url = new URL(src);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                InputStream input = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(input);
+                return myBitmap;
+            } catch (IOException e) {
+                // Log exception
+                return null;
+            }
+        }
     }
 }
--- app/src/main/java/com/example/readify/Profile/ProfileFragment.java
+++ app/src/main/java/com/example/readify/Profile/ProfileFragment.java
@@ -1,8 +1,13 @@
 package com.example.readify.Profile;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.core.content.ContextCompat;
@@ -10,6 +15,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,6 +28,7 @@ import com.example.readify.Adapters.AchievementsHoritzontalAdapter;
 import com.example.readify.Adapters.BooksHorizontalAdapter;
 import com.example.readify.Adapters.BooksProfileHoritzontalAdapter;
 import com.example.readify.Adapters.GenresHoritzontalAdapter;
+import com.example.readify.Login.LoginActivity;
 import com.example.readify.MainActivity;
 import com.example.readify.MockupsValues;
 import com.example.readify.Models.Achievement;
@@ -31,6 +38,11 @@ import com.example.readify.Models.User;
 import com.example.readify.Pages;
 import com.example.readify.R;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -57,6 +69,7 @@ public class ProfileFragment extends Fragment implements BooksProfileHoritzontal
     private User user;
 
     private OnFragmentInteractionListener mListener;
+    private SharedPreferences prefs;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -94,15 +107,21 @@ public class ProfileFragment extends Fragment implements BooksProfileHoritzontal
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
-        user = MockupsValues.getUser();
+        prefs = getActivity().getSharedPreferences("com.example.readify", Context.MODE_PRIVATE);
+        user = MockupsValues.getUserProfile();
 
         // Put the name of the user
         CircleImageView userImage = (CircleImageView) view.findViewById(R.id.profile_image);
-        userImage.setImageResource(
-                getContext().getResources().getIdentifier(MockupsValues.user.getPicture(), "drawable", getContext().getPackageName()));
+        byte[] imageAsBytes = Base64.decode(prefs.getString("com.example.readify.photo", "").getBytes(), Base64.DEFAULT);
+        userImage.setImageBitmap(BitmapFactory.decodeByteArray(imageAsBytes, 0, imageAsBytes.length));
+        //String image = prefs.getString("com.example.readify.photo", MockupsValues.user.getPicture());
+        //userImage.setImageBitmap(getBitmapFromURL(image));
+            //userImage.setImageResource(
+             //       getContext().getResources().getIdentifier(image, "drawable", getContext().getPackageName()));
+
 
         TextView textViewNameUser = (TextView) view.findViewById(R.id.nameUserTextview);
-        textViewNameUser.setText(user.getName());
+        textViewNameUser.setText(prefs.getString("com.example.readify.name", "User Unknown"));
 
         // Change the layout accord to the type of account
         final ImageView imageViewPremiumBadge = (ImageView) view.findViewById(R.id.premiumBadgeProfile);
@@ -149,8 +168,9 @@ public class ProfileFragment extends Fragment implements BooksProfileHoritzontal
         final AchievementsHoritzontalAdapter adapterAchievements = new AchievementsHoritzontalAdapter(getContext(), achievementsCompleted);
         recyclerViewAchievements.setAdapter(adapterAchievements);
 
-        Button button = (Button) view.findViewById(R.id.upgrade_button);
-        button.setOnClickListener(
+        //Upgrade account button
+        Button buttonUpgrade = (Button) view.findViewById(R.id.upgrade_button);
+        buttonUpgrade.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
@@ -162,6 +182,15 @@ public class ProfileFragment extends Fragment implements BooksProfileHoritzontal
                     }
                 }
         );
+
+        Button disconnectButton = (Button) view.findViewById(R.id.logOutButton);
+        disconnectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity activity = (MainActivity) getActivity();
+                activity.logOut();
+            }
+        });
 
         return view;
     }
